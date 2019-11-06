3a1e5b716b703778f900813b234bc750894389d5
fabs200 <fabs200@users.noreply.github.com>
2019-11-06 16:27:42 +0100

    Update bug fixes, iloc, NAs if no entries, json placeholder

--- Python/GetGeocodes.py
+++ Python/GetGeocodes.py
@@ -12,7 +12,7 @@ file = 'branches_address.csv'
 geodata = pd.DataFrame(pd.read_csv(path + file))
 
 # Specify class to get data from API US geocodes
-class getgeocodes:
+class GetGeocodeJSON:
     """
     returns json after specifying address in the format '4600 Silver Hill Rd, Suitland, MD 20746”'
     """
@@ -31,44 +31,139 @@ class getgeocodes:
 
     def get_json(self):
         address = self.address
-        url = getgeocodes(address).make_url()
+        url = GetGeocodeJSON(address).make_url()
         data = requests.get(url).text
-        json = js.loads(data)
+        # If no json could be retrieved, make an empty json
+        try:
+            json = js.loads(data)
+        except:
+            json = {'result': {'addressMatches': []}}
         return json
 
     def get_input(self):
         address = self.address
-        return getgeocodes(address).get_json()['result']['input']
+        return GetGeocodeJSON(address).get_json()['result']['input']
 
     def get_matched_address(self):
         address = self.address
-        return getgeocodes(address).get_json()['result']['addressMatches'][0]['matchedAddress']
+        return GetGeocodeJSON(address).get_json()['result']['addressMatches'][0]['matchedAddress']
 
     def get_coordinates(self):
         address = self.address
-        return getgeocodes(address).get_json()['result']['addressMatches'][0]['coordinates']
+        return GetGeocodeJSON(address).get_json()['result']['addressMatches'][0]['coordinates']
 
     def get_tigerLine(self):
         address = self.address
-        return getgeocodes(address).get_json()['result']['addressMatches'][0]['tigerLine']
+        return GetGeocodeJSON(address).get_json()['result']['addressMatches'][0]['tigerLine']
 
     def get_addressComponents(self):
         address = self.address
-        return getgeocodes(address).get_json()['result']['addressMatches'][0]['addressComponents']
+        return GetGeocodeJSON(address).get_json()['result']['addressMatches'][0]['addressComponents']
 
     def get_CensusBlock(self, keyword=None):
         address = self.address
         self.keyword = keyword
         if keyword is None:
-            return getgeocodes(address).get_json()['result']['addressMatches'][0]['geographies']
+            return GetGeocodeJSON(address).get_json()['result']['addressMatches'][0]['geographies']
         if keyword=='States':
-            return getgeocodes(address).get_json()['result']['addressMatches'][0]['geographies']['States'][0]
+            return GetGeocodeJSON(address).get_json()['result']['addressMatches'][0]['geographies']['States'][0]
         if keyword=='Counties':
-            return getgeocodes(address).get_json()['result']['addressMatches'][0]['geographies']['Counties'][0]
+            return GetGeocodeJSON(address).get_json()['result']['addressMatches'][0]['geographies']['Counties'][0]
         if keyword=='Census Tracts':
-            return getgeocodes(address).get_json()['result']['addressMatches'][0]['geographies']['Census Tracts'][0]
+            return GetGeocodeJSON(address).get_json()['result']['addressMatches'][0]['geographies']['Census Tracts'][0]
         if keyword=='Census Blocks':
-            return getgeocodes(address).get_json()['result']['addressMatches'][0]['geographies']['Census Blocks'][0]
+            return GetGeocodeJSON(address).get_json()['result']['addressMatches'][0]['geographies']['Census Blocks'][0]
+
+def get_Geodata(geodataJSON, keyword=None):
+    if keyword is None:
+        return geodataJSON
+
+    # First check whether matchedAddress is non-empty
+        # first check if empty = no result
+        if geodataJSON['result']['addressMatches'] == [] is True:
+            return 'NA'
+        else:
+            # matchedAddress
+            if keyword=='matchedAddress':
+                return geodataJSON['result']['addressMatches'][0]['matchedAddress']
+            if keyword=='x_coord':
+                return geodataJSON['result']['addressMatches'][0]['coordinates']['x']
+            if keyword=='y_coord':
+                return geodataJSON['result']['addressMatches'][0]['coordinates']['y']
+            if keyword=='tigerLine':
+                return geodataJSON['result']['addressMatches'][0]['tigerLine']
+
+            # addressComponent
+            if keyword=='fromAddress':
+                return geodataJSON['result']['addressMatches'][0]['addressComponents']['fromAddress']
+            if keyword=='preQualifier':
+                return geodataJSON['result']['addressMatches'][0]['addressComponents']['preQualifier']
+            if keyword=='preDirection':
+                return geodataJSON['result']['addressMatches'][0]['addressComponents']['preDirection']
+            if keyword=='preType':
+                return geodataJSON['result']['addressMatches'][0]['addressComponents']['preType']
+            if keyword=='streetName':
+                return geodataJSON['result']['addressMatches'][0]['addressComponents']['streetName']
+            if keyword=='suffixType':
+                return geodataJSON['result']['addressMatches'][0]['addressComponents']['suffixType']
+            if keyword=='suffixDirection':
+                return geodataJSON['result']['addressMatches'][0]['addressComponents']['suffixDirection']
+            if keyword=='suffixQualifier':
+                return geodataJSON['result']['addressMatches'][0]['addressComponents']['suffixQualifier']
+            if keyword=='city':
+                return geodataJSON['result']['addressMatches'][0]['addressComponents']['city']
+            if keyword=='state':
+                return geodataJSON['result']['addressMatches'][0]['addressComponents']['state']
+            if keyword=='zip':
+                return geodataJSON['result']['addressMatches'][0]['addressComponents']['zip']
+
+            # States
+            if keyword=='STATENS':
+                return geodataJSON['result']['addressMatches'][0]['geographies']['States'][0]['STATENS']
+
+            # Census Tracts
+            if keyword=='POP100':
+                return geodataJSON['result']['addressMatches'][0]['geographies']['Census Tracts'][0]['POP100']
+            if keyword=='GEOID':
+                return geodataJSON['result']['addressMatches'][0]['geographies']['Census Tracts'][0]['GEOID']
+            if keyword=='CENTLAT':
+                return geodataJSON['result']['addressMatches'][0]['geographies']['Census Tracts'][0]['CENTLAT']
+            if keyword=='AREAWATER':
+                return geodataJSON['result']['addressMatches'][0]['geographies']['Census Tracts'][0]['AREAWATER']
+            if keyword=='STATE':
+                return geodataJSON['result']['addressMatches'][0]['geographies']['Census Tracts'][0]['STATE']
+            if keyword=='BASENAME':
+                return geodataJSON['result']['addressMatches'][0]['geographies']['Census Tracts'][0]['BASENAME']
+            if keyword=='OID':
+                return geodataJSON['result']['addressMatches'][0]['geographies']['Census Tracts'][0]['OID']
+            if keyword=='LSADC':
+                return geodataJSON['result']['addressMatches'][0]['geographies']['Census Tracts'][0]['LSADC']
+            if keyword=='FUNCSTAT':
+                return geodataJSON['result']['addressMatches'][0]['geographies']['Census Tracts'][0]['FUNCSTAT']
+            if keyword=='INTPLAT':
+                return geodataJSON['result']['addressMatches'][0]['geographies']['Census Tracts'][0]['INTPLAT']
+            if keyword=='NAME':
+                return geodataJSON['result']['addressMatches'][0]['geographies']['Census Tracts'][0]['NAME']
+            if keyword=='OBJECTID':
+                return geodataJSON['result']['addressMatches'][0]['geographies']['Census Tracts'][0]['OBJECTID']
+            if keyword=='TRACT':
+                return geodataJSON['result']['addressMatches'][0]['geographies']['Census Tracts'][0]['TRACT']
+            if keyword=='CENTLON':
+                return geodataJSON['result']['addressMatches'][0]['geographies']['Census Tracts'][0]['CENTLON']
+            if keyword=='HU100':
+                return geodataJSON['result']['addressMatches'][0]['geographies']['Census Tracts'][0]['HU100']
+            if keyword=='AREALAND':
+                return geodataJSON['result']['addressMatches'][0]['geographies']['Census Tracts'][0]['AREALAND']
+            if keyword=='INTPTLON':
+                return geodataJSON['result']['addressMatches'][0]['geographies']['Census Tracts'][0]['INTPTLON']
+            if keyword=='MTFCC':
+                return geodataJSON['result']['addressMatches'][0]['geographies']['Census Tracts'][0]['MTFCC']
+            if keyword=='UR':
+                return geodataJSON['result']['addressMatches'][0]['geographies']['Census Tracts'][0]['UR']
+
+            # Census Block
+            if keyword=='COUNTY':
+                return geodataJSON['result']['addressMatches'][0]['geographies']['Census Blocks'][0]['COUNTY']
 
 # Add new cols
 geodata['matchedAddress'] = ''
@@ -87,7 +182,6 @@ geodata['city_addressComponents'] = ''
 geodata['state_addressComponents'] = ''
 geodata['zip_addressComponents'] = ''
 geodata['STATENS'] = ''
-geodata['COUNTY=Counties'] = ''
 geodata['POP100'] = ''
 geodata['GEOID'] = ''
 geodata['CENTLAT'] = ''
@@ -109,56 +203,86 @@ geodata['MTFCC'] = ''
 geodata['UR'] = ''
 geodata['COUNTY'] = ''
 
-# Loop over addresses and get new data
-for i in range(0, geodata.size):
+### Loop over addresses and get new data
 
-    # print
-    progress = 100*(i/geodata.size)
-    print(progress)
+# TEMPÖ Make 10 parts
+k = 10
+t = int(1/k * geodata.size)
+p1 = range(0, t)
+p2 = range(t, t+2)
+p3 = range(t+3, t+4)
+p4 = range(t+4, t+5)
+p5 = range(t+5, t+6)
+p6 = range(t+6, t+7)
+p7 = range(t+7, t+8)
+p8 = range(t+8, t+9)
+p9 = range(t+9, t+10)
+p10 = range(t+10, geodata.size)
+
+
+# modulus counter, once it reaches 250, geodata is saved and moduluscounter is set back to 0
+moduluscounter=0
+
+# for i in range(0, geodata.size):
+for i in p1:
 
     # Set up address to search for
     searchaddress = geodata['street'].iloc[i] + ' ' + geodata['city'].iloc[i] + ', ' + geodata['state'].iloc[i] + ' ' + str(geodata['zip'].iloc[i])
+
+    # print
+    progress = round(100*(i/geodata.size), 5)
+    print('observation {}/{} - {}% - search address: {}'.format(i, geodata.size, progress, searchaddress))
+
     # Download json
-    geocode = getgeocodes(address=searchaddress)
+    json = GetGeocodeJSON(address=searchaddress).get_json()
+
     # Save Infos
-    geodata[i, 'matchedAddress'] = geocode.get_matched_address()
-    geodata[i, 'x_coord'] = geocode.get_coordinates()['x']
-    geodata[i, 'y_coord'] = geocode.get_coordinates()['y']
-    geodata[i, 'fromAddress_addressComponents'] = geocode.get_addressComponents()['fromAddress']
-    geodata[i, 'toAddress_addressComponents'] = geocode.get_addressComponents()['toAddress']
-    geodata[i, 'preQualifier_addressComponents'] = geocode.get_addressComponents()['preQualifier']
-    geodata[i, 'preDirection_addressComponents'] = geocode.get_addressComponents()['preDirection']
-    geodata[i, 'preType_addressComponents'] = geocode.get_addressComponents()['preType']
-    geodata[i, 'streetName_addressComponents'] = geocode.get_addressComponents()['streetName']
-    geodata[i, 'suffixType_addressComponents'] = geocode.get_addressComponents()['suffixType']
-    geodata[i, 'suffixDirection_addressComponents'] = geocode.get_addressComponents()['suffixDirection']
-    geodata[i, 'suffixQualifier_addressComponents'] = geocode.get_addressComponents()['suffixQualifier']
-    geodata[i, 'city_addressComponents'] = geocode.get_addressComponents()['city']
-    geodata[i, 'state_addressComponents'] = geocode.get_addressComponents()['state']
-    geodata[i, 'zip_addressComponents'] = geocode.get_addressComponents()['zip']
-    geodata[i, 'STATENS'] = geocode.get_CensusBlock(keyword='States')['STATENS']
-    geodata[i, 'COUNTY'] = geocode.get_CensusBlock(keyword='Counties')['COUNTY']
-    geodata[i, 'POP100'] = geocode.get_CensusBlock(keyword='Census Tracts')['POP100']
-    geodata[i, 'GEOID'] = geocode.get_CensusBlock(keyword='Census Tracts')['GEOID']
-    geodata[i, 'CENTLAT'] = geocode.get_CensusBlock(keyword='Census Tracts')['CENTLAT']
-    geodata[i, 'AREAWATER'] = geocode.get_CensusBlock(keyword='Census Tracts')['AREAWATER']
-    geodata[i, 'STATE'] = geocode.get_CensusBlock(keyword='Census Tracts')['STATE']
-    geodata[i, 'BASENAME'] = geocode.get_CensusBlock(keyword='Census Tracts')['BASENAME']
-    geodata[i, 'OID'] = geocode.get_CensusBlock(keyword='Census Tracts')['OID']
-    geodata[i, 'LSADC'] = geocode.get_CensusBlock(keyword='Census Tracts')['LSADC']
-    geodata[i, 'FUNCSTAT'] = geocode.get_CensusBlock(keyword='Census Tracts')['FUNCSTAT']
-    geodata[i, 'INTPLAT'] = geocode.get_CensusBlock(keyword='Census Tracts')['INTPLAT']
-    geodata[i, 'NAME'] = geocode.get_CensusBlock(keyword='Census Tracts')['NAME']
-    geodata[i, 'OBJECTID'] = geocode.get_CensusBlock(keyword='Census Tracts')['OBJECTID']
-    geodata[i, 'TRACT'] = geocode.get_CensusBlock(keyword='Census Tracts')['TRACT']
-    geodata[i, 'CENTLON'] = geocode.get_CensusBlock(keyword='Census Tracts')['CENTLON']
-    geodata[i, 'HU100'] = geocode.get_CensusBlock(keyword='Census Tracts')['HU100']
-    geodata[i, 'AREALAND'] = geocode.get_CensusBlock(keyword='Census Tracts')['AREALAND']
-    geodata[i, 'INTPTLON'] = geocode.get_CensusBlock(keyword='Census Tracts')['INTPTLON']
-    geodata[i, 'MTFCC'] = geocode.get_CensusBlock(keyword='Census Tracts')['MTFCC']
-    geodata[i, 'UR'] = geocode.get_CensusBlock(keyword='Census Tracts')['UR']
+    geodata.loc[i, 'matchedAddress'] = get_Geodata(json, keyword='matchedAddress')
+    geodata.loc[i, 'x_coord'] = get_Geodata(json, keyword='x_coord')
+    geodata.loc[i, 'y_coord'] = get_Geodata(json, keyword='y_coord')
+    geodata.loc[i, 'fromAddress_addressComponents'] = get_Geodata(json, keyword='fromAddress')
+    geodata.loc[i, 'toAddress_addressComponents'] = get_Geodata(json, keyword='toAddress')
+    geodata.loc[i, 'preQualifier_addressComponents'] = get_Geodata(json, keyword='preQualifier')
+    geodata.loc[i, 'preDirection_addressComponents'] = get_Geodata(json, keyword='preDirection')
+    geodata.loc[i, 'preType_addressComponents'] =get_Geodata(json, keyword='preType')
+    geodata.loc[i, 'streetName_addressComponents'] = get_Geodata(json, keyword='streetName')
+    geodata.loc[i, 'suffixType_addressComponents'] = get_Geodata(json, keyword='suffixType')
+    geodata.loc[i, 'suffixDirection_addressComponents'] = get_Geodata(json, keyword='suffixDirection')
+    geodata.loc[i, 'suffixQualifier_addressComponents'] = get_Geodata(json, keyword='suffixQualifier')
+    geodata.loc[i, 'city_addressComponents'] = get_Geodata(json, keyword='city')
+    geodata.loc[i, 'state_addressComponents'] = get_Geodata(json, keyword='state')
+    geodata.loc[i, 'zip_addressComponents'] = get_Geodata(json, keyword='zip')
+    geodata.loc[i, 'STATENS'] = get_Geodata(json, keyword='STATENS')
+    geodata.loc[i, 'COUNTY'] = get_Geodata(json, keyword='COUNTY')
+    geodata.loc[i, 'POP100'] = get_Geodata(json, keyword='POP100')
+    geodata.loc[i, 'GEOID'] = get_Geodata(json, keyword='GEOID')
+    geodata.loc[i, 'CENTLAT'] = get_Geodata(json, keyword='CENTLAT')
+    geodata.loc[i, 'AREAWATER'] = get_Geodata(json, keyword='AREAWATER')
+    geodata.loc[i, 'STATE'] = get_Geodata(json, keyword='STATE')
+    geodata.loc[i, 'BASENAME'] = get_Geodata(json, keyword='BASENAME')
+    geodata.loc[i, 'OID'] = get_Geodata(json, keyword='OID')
+    geodata.loc[i, 'LSADC'] = get_Geodata(json, keyword='LASADC')
+    geodata.loc[i, 'FUNCSTAT'] = get_Geodata(json, keyword='FUNCSTAT')
+    geodata.loc[i, 'INTPLAT'] = get_Geodata(json, keyword='INTPLANT')
+    geodata.loc[i, 'NAME'] = get_Geodata(json, keyword='NAME')
+    geodata.loc[i, 'OBJECTID'] = get_Geodata(json, keyword='OBJECTID')
+    geodata.loc[i, 'TRACT'] = get_Geodata(json, keyword='TRACT')
+    geodata.loc[i, 'CENTLON'] = get_Geodata(json, keyword='CENTLON')
+    geodata.loc[i, 'HU100'] = get_Geodata(json, keyword='HU100')
+    geodata.loc[i, 'AREALAND'] = get_Geodata(json, keyword='AREALAND')
+    geodata.loc[i, 'INTPTLON'] = get_Geodata(json, keyword='INTPTLON')
+    geodata.loc[i, 'MTFCC'] = get_Geodata(json, keyword='MTFCC')
+    geodata.loc[i, 'UR'] = get_Geodata(json, keyword='UR')
 
-# Export data
-geodata.to_csv(path + 'branches_address_new.csv', index=None, header=True)
-geodata.to_excel(path + 'branches_address_new.xlsx', sheet_name='Geo Data', header=True)
+    # Export data each i=250 entries
+    moduluscounter += 1
+    if moduluscounter == 250:
 
+        # Display message for saving
+        print('saving file for first {} entries'.format(i))
+
+        # Export data
+        geodata.to_csv(path + 'branches_address_new_p1.csv', index=None, header=True)
+        geodata.to_excel(path + 'branches_address_new_p1.xlsx', sheet_name='Geo Data', header=True)
+        # Set back to 0
+        moduluscounter = 0
